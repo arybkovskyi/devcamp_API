3cf74846acc7db21b385ce4251c64169d992dc2b
arybkovskyi <79677544+arybkovskyi@users.noreply.github.com>
2022-04-21 01:48:45 -0500

    added mondoDB, mongoose (#1)

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db')
 
 //route files
 const bootcamps = require('./routes/bootcamps')
 
 dotenv.config({path: './config/config.env'});
+
+//connect database
+connectDB()
 
 const app = express();
 
@@ -20,4 +24,13 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT} `))
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT} `))
+
+//handle unhandled rejections
+process.on('unhandledRejection', (err, promise)=>{
+    console.log(`Error: ${err.message}`)
+    //close server and exit process
+    server.close(() =>{
+        process.exit(1)
+    })
+})
